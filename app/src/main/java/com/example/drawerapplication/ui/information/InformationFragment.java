e9599f49d60420ecc131ee4cfd246aec8bd8135c
Andrew <powercode17@gmail.com>
2021-10-18 15:00:22 +0800

    getCurrentTime

--- app/src/main/java/com/example/drawerapplication/ui/information/InformationFragment.java
+++ app/src/main/java/com/example/drawerapplication/ui/information/InformationFragment.java
@@ -102,6 +102,7 @@ public class InformationFragment extends Fragment {
     @RequiresApi(api = Build.VERSION_CODES.N)
     private String getCurrentTime(){
         return new SimpleDateFormat("hh:mm a", Locale.getDefault()).format(new Date());
+        //dito kutaDrew
     }
 
 }
